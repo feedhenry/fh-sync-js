890d84951354207f87f6a3790edf6a3b9ea8c0ca
MikeyBurkman <michael.burkman@gmail.com>
2017-11-07 15:58:28 -0500

    Sync factory function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ var api_sync = require("./sync-client");
 
 // Mounting into global fh namespace
 var fh = window.$fh = window.$fh || {};
-fh.sync = api_sync;
+fh.sync = api_sync();
+fh.syncFactory = api_sync;
 
 module.exports = fh.sync;
