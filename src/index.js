7c7ffde635c32714ab15b7f46e52739aa9277284
MikeyBurkman <michael.burkman@gmail.com>
2017-11-21 15:31:31 -0500

    Fix unit tests and TS typings

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ var api_sync = require("./sync-client");
 // Mounting into global fh namespace
 var fh = window.$fh = window.$fh || {};
 fh.sync = api_sync();
-fh.syncFactory = api_sync;
+fh.sync.factory = api_sync;
 
 module.exports = fh.sync;
